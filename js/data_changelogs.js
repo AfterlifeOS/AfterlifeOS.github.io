976d59ec15ea3b4a12108f89d9a74085e11562b6
Mr_ZerO <71553416+RdW-afk@users.noreply.github.com>
2026-02-28 12:42:14 +0700

    [CMS] Release AfterlifeOS V 8.3 LatsBlood

---
+++ js/data_changelogs.js
@@ -0,0 +1,8 @@
+[
+    {
+        "version": "AfterlifeOS V 8.3 LatsBlood",
+        "date": "February 27, 2026",
+        "isLatest": true,
+        "changes": "\n- Initial android 16 QPR 2 release\n"
+    }
+]
