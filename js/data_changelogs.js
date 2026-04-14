578b2e4bb9ce9a1bf94776ce1f78604aac512148
Mr_ZerO <71553416+RdW-afk@users.noreply.github.com>
2026-04-14 23:17:52 +0700

    [CMS] Release AfterlifeOS V8.4 Ophelia

--- js/data_changelogs.js
+++ js/data_changelogs.js
@@ -1,8 +1,14 @@
 [
     {
+        "version": "AfterlifeOS V8.4 Ophelia",
+        "date": "April 14, 2026",
+        "isLatest": true,
+        "changes": "\nAndroid 16 QPR2 April Update\nImprove Lify QS, Fix Animation, Memory Leak and Drain Battery\nAdd mini visualizer at media player qs\nOptional to show and hide albumArt on media qs\nBring back OOS Clock QS\nAdding dynamic font feature\nAdd Wi-Fi and Bluetooth timeout feature\nAdd Wi-Fi standart icon feature\nAdd ability to hide carrier name on lockscreen\nBring back Monet feature\nAdd lockscreen media art\nAdd pulse visualizer\nAdd kill button to notification guts\nIntegrate Google Lens into Screenshot UI\nImplement burn-in protection for statusbar\nIntroduce Lockscreen Now playing\nIntroduce Reality Display engine\nAdd notification row transluency toggle\nAdd support for hide applist\nIntroduce cutout progress ring \nBring back lockscreen Clock feature\nRefactor ongoing action progress chip feature\nImplement device idle manager\nVarious improvements and optimizations\n"
+    },
+    {
         "version": "AfterlifeOS V 8.3 LatsBlood",
         "date": "February 27, 2026",
-        "isLatest": true,
+        "isLatest": false,
         "changes": "\n- Initial android 16 QPR 2 release\n"
     }
 ]
